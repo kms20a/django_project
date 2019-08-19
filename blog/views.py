bde5df2e3ab7ddf2697acf53c27191382e504319
kms20a <kms21@naver.com>
2019-08-20 01:40:03 +0900

    add - HTML Render

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+#dummy data
+posts = [
+    {
+        'author': 'Mskim',
+        'title': 'Blog Post1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Mskim',
+        'title': 'Blog Post2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+    
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home<h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About<h1>')
+    return render(request, 'blog/about.html')
